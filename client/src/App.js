7c6f741587f554fc839d0c42ed157818ea2c8a3b
rootFaheem <faheemtech1@gmail.com>
2019-12-02 12:21:05 +0530

    frontend changed to simple login and chat

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
-import home from "./home";
-import chat from "./app/chat/chat";
+// import home from "./home";
+// import chat from "./app/chat/chat";
+
+import Join from "./app/components/Join";
+import Chat from "./app/components/Chat";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={home}></Route>
-          <Route exact path="/chat" component={chat}></Route>
+          <Route exact path="/" component={Join}></Route>
+          <Route exact path="/chat" component={Chat}></Route>
         </Switch>
       </div>
     </BrowserRouter>
