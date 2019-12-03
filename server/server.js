eca92cf6cf9b8f33e4ced4de13a907e2644f6052
rootFaheem <faheemtech1@gmail.com>
2019-12-03 13:56:07 +0530

    sendMessage emit from backend is handled

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const {
 } = require("./app/users/users.utils");
 
 io.on("connection", socket => {
+  // ADDING NEW USER AND SENDING WELCOME MESSAGE TO USER AND NOTIFICATION TO OTHERS
   socket.on("join", ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
@@ -31,6 +32,15 @@ io.on("connection", socket => {
       .emit("message", { user: "admin", text: `${user.name} has joined!` });
 
     socket.join(user.room);
+
+    callback();
+  });
+
+  // SENDING THE MESSAGE TO ROOM EMITTED FROM A USER
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
 
     callback();
   });
