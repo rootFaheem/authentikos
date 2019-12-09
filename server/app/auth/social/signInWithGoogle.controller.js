9cb25602a093ad359819b9ed6a6d81a2a4886440
rootFaheem <faheemtech1@gmail.com>
2019-12-10 00:34:42 +0530

    working with some changes

--- server/app/auth/social/signInWithGoogle.controller.js
+++ server/app/auth/social/signInWithGoogle.controller.js
@@ -1,6 +1,8 @@
 const { google } = require("googleapis");
-
+const express = require("express");
 const OAuth2Data = require("../../../configs/clientSecret.json");
+
+const app = express();
 
 const CLIENT_ID = OAuth2Data.web.client_id;
 const CLIENT_SECRET = OAuth2Data.web.client_secret;
@@ -11,75 +13,57 @@ const oAuth2Client = new google.auth.OAuth2(
   CLIENT_SECRET,
   REDIRECT_URL
 );
-const authed = false;
+var authed = false;
 
-const signinWithGoogle = async (req, res, next) => {
-  console.log(CLIENT_ID);
-  console.log(CLIENT_SECRET);
-  console.log(REDIRECT_URL);
-  try {
-    if (!authed) {
-      const url = oAuth2Client.generateAuthUrl({
-        access_type: "offline",
-        scope: "https://www.googleapis.com/auth/gmail.readonly"
-      });
-
-      console.log(url);
-      return res.status(200).json({
-        success: true,
-        url
-      });
-    }
-
-    const gmail = google.gmail({ version: "v1", auth: "oAuth2Client" });
-
+app.get("/", (req, res) => {
+  if (!authed) {
+    // Generate an OAuth URL and redirect there
+    const url = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: "https://www.googleapis.com/auth/gmail.readonly"
+    });
+    console.log(url);
+    res.redirect(url);
+  } else {
+    const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
     gmail.users.labels.list(
       {
         userId: "me"
       },
       (err, res) => {
-        if (err) return console.log("API returned an error", err);
-
+        if (err) return console.log("The API returned an error: " + err);
         const labels = res.data.labels;
-
         if (labels.length) {
-          console.log("Labels: ");
+          console.log("Labels:");
           labels.forEach(label => {
             console.log(`- ${label.name}`);
           });
         } else {
-          console.log("No label found");
+          console.log("No labels found.");
         }
       }
     );
-
-    res.send("logged in");
-  } catch (err) {
-    next(err);
+    res.send("Logged in");
   }
-};
+});
 
-const googleAuthCallback = async (req, res) => {
+app.get("/auth/google/callback", function(req, res) {
   const code = req.query.code;
-  try {
-    if (code) {
-      oAuth2Client.getToken(code, (err, tokens) => {
-        if (err) {
-          console.log("Error Authenticating:", err);
-        }
-
-        console.log("Successfully Authenticated");
+  if (code) {
+    // Get an access token based on our OAuth code
+    oAuth2Client.getToken(code, function(err, tokens) {
+      if (err) {
+        console.log("Error authenticating");
+        console.log(err);
+      } else {
+        console.log("Successfully authenticated");
         oAuth2Client.setCredentials(tokens);
         authed = true;
-        res.status(200).json({
-          success: true,
-          authed
-        });
-      });
-    }
-  } catch (err) {
-    next(err);
+        res.redirect("/");
+      }
+    });
   }
-};
+});
 
-module.exports = signinWithGoogle;
+const port = process.env.port || 5000;
+app.listen(port, () => console.log(`Server running at ${port}`));
--- server/app/auth/social/socialAuth.routes.js
+++ server/app/auth/social/socialAuth.routes.js
@@ -2,6 +2,6 @@ const Router = require("express").Router();
 
 const signinWithGoogleController = require("./signInWithGoogle.controller");
 
-Router.post("/signin-with-google", signinWithGoogleController);
+// Router.post("/signin-with-google", signinWithGoogleController);
 
 module.exports = Router;
